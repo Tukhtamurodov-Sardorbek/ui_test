47bd2f0854b337cbda11e1b0a6d9914372494d81
Tukhtamurodov Sardor <s.tukhtamurodov>
2022-11-22 15:54:17 +0500

    Code Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ui_test/shape_border.dart';
-import 'last_reponsive_shape.dart';
+import 'reponsive_shape.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,54 +54,6 @@ class ReceiptPage extends StatelessWidget {
             child: const Icon(Icons.account_circle),
           ),
         ),
-      ),
-    );
-
-    // CustomScrollView(
-    //   slivers: [
-    //     const ReceiptAppBar(),
-    //     ReceiptBody(transfer: transfer),
-    //   ],
-    // );
-  }
-}
-
-class ReceiptAppBar extends StatelessWidget {
-  const ReceiptAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverAppBar(
-      elevation: 8,
-      pinned: true,
-      expandedHeight: kToolbarHeight,
-      automaticallyImplyLeading: false,
-      title: const Text('Kvitansiya'),
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-    );
-  }
-}
-
-class ReceiptBody extends StatelessWidget {
-  const ReceiptBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SliverToBoxAdapter(
-      child:  Container(
-        height: 500.0,
-        decoration: ShapeDecoration(
-          color: Colors.white,
-          shape: MyShapeBorder(),
-
-          shadows: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              blurRadius: 6,
-            ),
-          ],
-        ),
-        child: const Center(child: Text("My Button", style: TextStyle(color: Colors.white))),
       ),
     );
   }
